9ac2dd077b9fc14560dcd8e56bcf797b53f51c4c
DeusSolis <Ibanusia@gmail.com>
2023-02-25 17:49:26 +0200

    add serializer for user

---
+++ myblog/myblog/user/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import UserModel
+
+
+class UserModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ['email', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
